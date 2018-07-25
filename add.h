65831120e0901973dc635f2883c193d19d8e940d
Leitrah <pyszczus2001@gmail.com>
2018-07-26 01:20:33 +0200

    Moje pierwsze repozytorium

---
+++ add.h
@@ -0,0 +1,7 @@
+#ifndef ADD_H
+#define ADD_H
+
+//tutaj stawiamy protopyp funkcji
+
+int add(int x, int y);
+#endif // ADD_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "add.h"
+
+int main()
+{
+    std::cout << "Obsluga wielu plikow " << add(5,6) << std::endl;
+    return 0;
+}
